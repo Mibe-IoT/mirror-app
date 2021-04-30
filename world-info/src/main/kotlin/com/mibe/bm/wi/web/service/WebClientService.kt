95be1286ae411b6386c69047923c6d1827d846c9
Ilya Buglakov <ilboogl@gmail.com>
2021-04-30 20:09:20 +0300

    add WebClientService properties

--- world-info/src/main/kotlin/com/mibe/bm/wi/web/service/WebClientService.kt
+++ world-info/src/main/kotlin/com/mibe/bm/wi/web/service/WebClientService.kt
@@ -1,13 +1,22 @@
 package com.mibe.bm.wi.web.service
 
 import com.fasterxml.jackson.databind.DeserializationFeature
+import com.mibe.bm.wi.files.FilePathService
+import com.mibe.bm.wi.files.FilePathServiceImpl
 import com.mibe.bm.wi.web.model.IpInfo
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.features.json.*
 import io.ktor.client.request.*
+import java.io.File
+import java.util.*
 
-class WebClientService {
+class WebClientService(
+    private val pathService: FilePathService
+) {
+
+    private val properties = Properties()
+    private val ipUrl: String
 
     val client: HttpClient = HttpClient(CIO) {
         install(JsonFeature) {
@@ -17,18 +26,23 @@ class WebClientService {
         }
     }
 
+    init {
+        properties.load(File(pathService.getFilePath("web.properties")).inputStream())
+        ipUrl = properties.getProperty("url.ip-resolve")
+    }
+
     suspend inline fun <reified T> requestForObject(url: String): T {
         return client.get(url)
     }
 
     suspend fun getIpInfo(): IpInfo {
-        return requestForObject<IpInfo>("https://ipapi.co/json")
+        properties.entries.forEach { println(it) }
+        return requestForObject(ipUrl)
     }
 
 }
 
 suspend fun main() {
-    val service = WebClientService()
-//    val ipInfo = service.requestForObject<IpInfo>("https://ipapi.co/json")
+    val service = WebClientService(FilePathServiceImpl())
     println(service.getIpInfo())
 }
